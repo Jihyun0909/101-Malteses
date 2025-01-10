e5245eb8af1d27a579840e204873bf64e4abbbd1
jihyun <149850518+Jihyun0909@users.noreply.github.com>
2025-01-10 13:31:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0){
+            answer += n % 10; //마지막 자릿수
+            n /= 10; //자릿수 줄이기
+        }
+
+        return answer;
+    }
+}
